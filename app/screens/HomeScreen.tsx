13c3cf8b2f43c1cc74f7c2995b73d8e205dbe212
Luís Lemos <lemosluis547@gmail.com>
2025-04-07 21:36:07 +0100

    🎉 Inicialização do projeto VoxLink com estrutura limpa

---
+++ app/screens/HomeScreen.tsx
@@ -0,0 +1,10 @@
+// app/screens/HomeScreen.tsx
+import { View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Bem-vindo ao VoxLink</Text>
+    </View>
+  );
+}
---
+++ app/screens/STTScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function STTScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>STT Screen</Text>
+    </View>
+  );
+}
